746c9d96bca7e98c06f25377f30bd81ad7ed681e
Sadat97 <moanwar1997@gmail.com>
2023-03-11 14:31:34 +0200

    remove the slack bot token

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Slack.configure do |config|
-  config.token = 'xoxb-4920316254455-4928249615142-oXiGWnzXA6FKl89F1j6Zf678' #ENV['SLACK_API_TOKEN']
+  config.token =ENV['SLACK_API_TOKEN']
 end
